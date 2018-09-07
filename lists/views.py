5a36927b0665beff67a8a3d4a84804c3f6db182b
Greg Shepley <gregshepley@yahoo.co.uk>
2018-09-07 10:58:18 +0100

    First unit test for posting data created

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home_page(request):
+    if request.method == 'POST':
+    	return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html')
