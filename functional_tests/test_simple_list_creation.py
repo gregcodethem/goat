c3f6d255a02c345256d5c482389aca356f107ecc
Greg Shepley <gregshepley@yahoo.co.uk>
2018-09-21 18:57:27 +0100

    rename all item input ids and names. Still broken

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -18,7 +18,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         # He's invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'), 'Enter a to-do item'
         )
@@ -33,7 +33,7 @@ class NewVisitorTest(FunctionalTest):
 
         # There is still a text box inviting him to add another item.
         # He enters: "Use feathers to make a fly"
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Use feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
@@ -47,7 +47,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Bob starts a new to-do list
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy feathers')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy feathers')
@@ -73,7 +73,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francesca starts a new list by entering
         # a new item.
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy Ostrich milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy Ostrich milk')
